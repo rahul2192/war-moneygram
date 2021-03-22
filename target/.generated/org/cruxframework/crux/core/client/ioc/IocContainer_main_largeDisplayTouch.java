8810bb5dba08efdcbcd0e8847e9eb16a71ae0b50
rahul2192 <rahul_verma2192@yahoo.com>
2021-03-22 04:18:32 -0400

    war-file moneygram

---
+++ target/.generated/org/cruxframework/crux/core/client/ioc/IocContainer_main_largeDisplayTouch.java
@@ -0,0 +1,26 @@
+package org.cruxframework.crux.core.client.ioc;
+
+import org.cruxframework.crux.core.client.screen.views.View;
+import com.google.gwt.core.client.GWT;
+
+public class IocContainer_main_largeDisplayTouch extends org.cruxframework.crux.core.client.ioc.IocContainer {
+  public IocContainer_main_largeDisplayTouch(View view){
+    super(view);
+  }
+  public  money.client.MainController.MainView getmoney_client_MainController_MainView(org.cruxframework.crux.core.client.ioc.IoCResource.Scope scope, String subscope){
+    money.client.MainController.MainView result = _getScope(scope).getValue(new org.cruxframework.crux.core.client.ioc.IocProvider<money.client.MainController.MainView>(){
+      public money.client.MainController.MainView get(){
+        return GWT.create(money.client.MainController.MainView.class);
+      }
+    }, "money.client.MainController.MainView", subscope, 
+    new IocScope.CreateCallback<money.client.MainController.MainView>(){
+      public void onCreate(money.client.MainController.MainView newObject){
+      }
+    }
+    );
+    if (scope != org.cruxframework.crux.core.client.ioc.IoCResource.Scope.SINGLETON && result.getBoundCruxViewId() == null){
+      result.bindCruxView(this.getBoundCruxViewId());
+    }
+    return result;
+  }
+}
